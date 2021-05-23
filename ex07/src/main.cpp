3a06218795b07fefac979699fb9a76467f1b8060
Ivan Rosic <ivarosic@z1r1p1.42lyon.fr>
2021-05-23 19:10:16 +0200

    presque fini l exo 07 et dernier exo

--- ex07/src/main.cpp
+++ ex07/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ivarosic <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/20 17:55:47 by ivarosic          #+#    #+#             */
-/*   Updated: 2021/05/20 18:59:45 by ivarosic         ###   ########lyon.fr   */
+/*   Updated: 2021/05/23 19:09:50 by ivarosic         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,59 @@ int main(int ac, char **av)
 		return(1);
 	}
 
-	std::string filename = av[1];
-	std::string s1 = av[2];
-	std::string s2 = av[3];
+	//std::string filename = av[1];
+	//std::string s1 = av[2];
+	//std::string s2 = av[3];
 
-	std::ifstream fs(filename);
-	if (!fs.good())
+	std::ifstream ifs1(av[2]);
+	if (!ifs1.good())
+	{
+		std::cout << "problem for open s1" << std::endl;
+		return(1);
+	}
+	std::string s1;
+	char c = ifs1.get();
+	while (ifs1.good())
+	{
+		s1 = s1 + c;
+		c = ifs1.get();
+	}
+	ifs1.close();
+
+	std::ifstream ifs2(av[3]);
+	if (!ifs2.good())
+	{
+		std::cout << "problem for open s2" << std::endl;
+		return(1);
+	}
+
+	std::string s2;
+	c = ifs2.get();
+
+	while (ifs2.good())
+	{
+		s2 = s2 + c;
+		c = ifs2.get();
+	}
+	ifs2.close();
+
+	if (s1.empty())
+	{
+		std::cout << "s1 is empty" << std::endl;
+		return(1);
+	}
+	if (s2.empty())
+	{
+		std::cout << "s2 is empty" << std::endl;
+		return(1);
+	}
+
+
+
+
+
+	std::ifstream ifs(av[1]);
+	if (!ifs.good())
 	{
 		std::cout << "problem for open FILENAME" << std::endl;
 		return(1);
